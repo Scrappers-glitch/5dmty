9bbc45e03552b05a7efaff7195359c368d722a65
Scrappers <scrappers.tm@gmail.com>
2020-09-24 14:10:35 -0500

    Clone

--- app/src/main/java/com/scrappers/churchService/localDatabase/LocalDatabase.java
+++ app/src/main/java/com/scrappers/churchService/localDatabase/LocalDatabase.java
@@ -17,7 +17,7 @@ public class LocalDatabase {
     private final AppCompatActivity context;
     private final String file;
 
-    //hi from your clone
+    //hi from your  clone
     /**
      * Instantiate a new JSON database local file
      * @param context the context of the activity
